360997b008e96736bdeab61932c29048ff69e84b
Rohan <rohansingh700976@gmail.com>
2026-04-15 12:00:23 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose');
 
 const connectDB=async()=>{
     try {
-        await mongoose.connect('mongodb+srv://kashyapshivanshu63:acnWBB9v6olumTGL@cluster0.zxu5x.mongodb.net/userdb?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect('DB_URL');
         console.log('mongo connected');
     } catch (error) {
         console.log("error occur",error);
